e230ffb9a1192f12741de0c2839a71bc9eb7a5cb
anirudh <anirudh05snair@gmail.com>
2026-02-28 12:23:48 +0530

    db_loader added

---
+++ db_loader.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+from dotenv import load_dotenv
+from sqlalchemy import URL, create_engine
+load_dotenv()
+
+data_set = pd.read_csv('online_retail_2010_2011.csv')
+
+connection_url = URL.create(
+    drivername="postgresql+psycopg2",
+    username=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD'),
+    host=os.getenv('DB_HOST'),
+    port=os.getenv('DB_PORT'),
+    database=os.getenv('DB_NAME')
+)
+
+engine = create_engine(connection_url)
+data_set.to_sql('online_retail_2010_2011', engine, if_exists='replace', index=False)
--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-import os
-from dotenv import load_dotenv
-import pandas as pd
-from sqlalchemy import URL, create_engine
-load_dotenv()
-data_set = pd.read_csv('online_retail_2010_2011.csv')
-connection_url = URL.create(
-    drivername="postgresql+psycopg2",
-    username=os.getenv('DB_USER'),
-    password=os.getenv('DB_PASSWORD'),
-    host=os.getenv('DB_HOST'),
-    port=os.getenv('DB_PORT'),
-    database=os.getenv('DB_NAME')
-)
 
-engine = create_engine(connection_url)
-data_set.to_sql('online_retail_2010_2011', engine, if_exists='replace', index=False)
+import pandas as pd
+
+data_set = pd.read_csv('online_retail_2010_2011.csv')
